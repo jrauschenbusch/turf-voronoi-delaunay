c10b161d52dad8121a818f69711f9a21a2b23e24
Jochen Rauschenbusch <jochen.rauschenbusch@gmail.com>
2021-08-14 08:35:41 +0200

    test: add keep properties test

--- src/index.test.ts
+++ src/index.test.ts
@@ -6,24 +6,48 @@ import path from 'path'
 import voronoiDelaunay from './index'
 import write from 'write-json-file'
 
-describe('turf-voronoi-delaunay', () => {
-  const files = glob.sync(path.join(__dirname, '..', 'test', 'in', '*.json'))
+describe('basic', () => {
+  const files = glob.sync(path.join(__dirname, '..', 'test', 'in', '!(*-keep).json'))
   for (const file of files) {
     const { name } = path.parse(file)
     it(name, () => {
+      // arrange
       const geojson = load.sync<FeatureCollection<Point>>(file)
-      const results = voronoiDelaunay(geojson, { bbox: geojson.bbox })
-
       const out = file.replace(
         path.join('test', 'in'),
         path.join('test', 'out')
       )
-
-      if (process.env.REGEN !== undefined) write.sync(out, results)
-
       const expected = load.sync(out)
 
+      // act
+      const results = voronoiDelaunay(geojson, { bbox: geojson.bbox })
+      if (process.env.REGEN !== undefined) write.sync(out, results)
+
+      // assert
       expect(JSON.stringify(results)).toEqual(JSON.stringify(expected))
+    })
+  }
+})
+
+describe('keep-properties', () => {
+  const files = glob.sync(path.join(__dirname, '..', 'test', 'in', '*-keep.json'))
+  for (const file of files) {
+    const { name } = path.parse(file)
+    it(name, () => {
+      // arrange
+      const geojson = load.sync<FeatureCollection<Point>>(file)
+      const out = file.replace(
+        path.join('test', 'in'),
+        path.join('test', 'out')
+      )
+      const expected = load.sync(out)
+
+      // act
+      const result = voronoiDelaunay(geojson, { keepProperties: true, bbox: geojson.bbox })
+      if (process.env.REGEN !== undefined) write.sync(out, result)
+
+      // assert
+      expect(JSON.stringify(result)).toEqual(JSON.stringify(expected))
     })
   }
 })
