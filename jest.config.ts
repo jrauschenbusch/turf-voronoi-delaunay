3a1aeac20cfc81e040fb6a4bcab8eb0c1a58dec2
Jochen Rauschenbusch <jochen.rauschenbusch@gmail.com>
2021-08-16 10:28:15 +0200

    chore: Enhance workflow

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ import type { Config } from '@jest/types'
 
 // Sync object
 const config: Config.InitialOptions = {
+  coverageReporters: ['lcov', 'text'],
   verbose: true,
   transformIgnorePatterns: [
     'node_modules/?!(d3-delaunay)'
