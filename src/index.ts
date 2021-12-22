89097c3202d375adbf47adc949f4235086025357
Jochen Rauschenbusch <jochen.rauschenbusch@gmail.com>
2021-12-22 10:02:53 +0100

    Fix for keep properties logic
    Fixes a bug where the keep properties option resulted in incorrectly assigned properties in relation to the source points.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
-import type { BBox, Feature, FeatureCollection, Point, Polygon } from '@turf/helpers'
+import type {
+  BBox,
+  Feature,
+  FeatureCollection,
+  Point,
+  Polygon,
+  Properties
+} from '@turf/helpers'
 import { featureCollection, polygon } from '@turf/helpers'
 
 import { Delaunay } from 'd3-delaunay'
@@ -17,9 +24,9 @@ interface Options {
  *
  * @name voronoi
  * @param {FeatureCollection<Point>} points to find the Voronoi polygons around.
- * @param {Options} [options={ keepProperties: false, bbox: [-180, -85, 180, -85] }] Optional parameters
+ * @param {Options} [options={ keepProperties: false, bbox: [-180, -85, 180, 85] }] Optional parameters
  * @param {boolean} [options.keepProperties=false] switch to enable/disable preservation of feature properties
- * @param {BBox} [options.bbox=[-180, -85, 180, -85]] clipping rectangle, in [minX, minY, maxX, MaxY] order.
+ * @param {BBox} [options.bbox=[-180, -85, 180, 85]] clipping rectangle, in [minX, minY, maxX, MaxY] order.
  * @returns {FeatureCollection<Polygon>} a set of polygons, one per input point.
  * @example
  * const options = {
@@ -31,22 +38,31 @@ interface Options {
  * //addToMap
  * const addToMap = [voronoiPolygons, points];
  */
-export function voronoi (points: FeatureCollection<Point>, options?: Options): FeatureCollection<Polygon> {
+export function voronoi (
+  points: FeatureCollection<Point>,
+  options?: Options
+): FeatureCollection<Polygon> {
   const bbox = options?.bbox ?? [-180, -85, 180, 85]
   if (!Array.isArray(bbox)) throw new Error('bbox is invalid')
   collectionOf(points, 'Point', 'points')
 
-  const fc = featureCollection(
-    Array.from(Delaunay.from<Feature<Point>>(points.features, feature => feature.geometry.coordinates[0], feature => feature.geometry.coordinates[1])
-      .voronoi([bbox[0], bbox[1], bbox[2], bbox[3]])
-      .cellPolygons())
-      .map((p: Delaunay.Polygon) => polygon([p])))
+  const polygonsIter = Delaunay.from<Feature<Point>>(
+    points.features,
+    (feature) => feature.geometry.coordinates[0],
+    (feature) => feature.geometry.coordinates[1]
+  )
+    .voronoi([bbox[0], bbox[1], bbox[2], bbox[3]])
+    .cellPolygons()
 
-  if (options?.keepProperties === true) {
-    fc.features.forEach((polygon: Feature, i: number) => {
-      polygon.properties = JSON.parse(JSON.stringify(points.features[i].properties))
-    })
+  const polygons: Array<Feature<Polygon, Properties>> = []
+  for (const value of polygonsIter) {
+    const properties =
+      options?.keepProperties === true
+        ? JSON.parse(JSON.stringify(points.features[value.index].properties))
+        : {}
+    const p = polygon([value], properties)
+    polygons.push(p)
   }
 
-  return fc
+  return featureCollection(polygons)
 }
